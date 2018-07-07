ae9a37441a02b58bb0435d74c79e8db4f73a0841
Bruno Gomes <jbg.bruno@gmail.com>
2018-07-07 12:00:43 -0300

    commit

--- PrimeiroProjeto/App.js
+++ PrimeiroProjeto/App.js
@@ -1,62 +1,81 @@
 import React, { Component } from 'React'
 import { View, Text, TouchableOpacity,Image, StyleSheet } from 'react-native'
 
+class Botao extends Component{
+  constructor(props){
+    super(props)
+    this.state = {}
+    let cols = 1
+    if(props.cols){
+      cols = parseInt(props.cols)
+    }
+    let bg = "#e0e0e0"
+    if(props.bg){
+      bg = props.bg
+    }
+    this.styles = StyleSheet.create({
+      area:{
+        flex: cols,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#999',
+        backgroundColor: bg
+      },
+      text:{
+        fontSize: 18,
+      }
+    })
+  }
+  render(){
+    return(
+      <TouchableOpacity style={this.styles.area}>
+        <Text style={this.styles.text}>{this.props.n}</Text>
+      </TouchableOpacity>
+    )
+  }
+}
 
 export default class PrimeiroProjeto extends Component {
   constructor(props){
     super(props)
-    this.state = {n : 0, botao: 'VAI'}
-    this.timer = null
-    this.vai = this.vai.bind(this)
-    this.limpar = this.limpar.bind(this)
     
   }
-  vai(){
-    let s = this.state;
-    
-
-    if(this.timer != null){
-      //parar o timer
-      clearInterval(this.timer)
-      this.timer = null
-      s.botao ="VAI"  
-    }else{
-      //começar o timer
-      this.timer = setInterval(()=>{
-        let s = this.state;
-        s.n += 0.1
-        this.setState(s)
-      }, 100)
-      s.botao ="PARAR"
-     
-    }
-    this.setState(s)
-  }
-  limpar(){
-    if(this.timer != null){
-      //parar o timer
-      clearInterval(this.timer)
-      this.timer = null
-    }
-    let s = this.state
-    s.n = 0
-    s.botao ="VAI"  
-    this.setState(s)
-
-  }
+  
   render() {
     return(
       <View style={styles.body}>
-          <Image style={styles.imagem} source={require('./images/relogio.png')}/>
-          <Text style={styles.tempoText}>{this.state.n.toFixed(1)}</Text>
-          <View style={styles.botaoArea}>
-            <TouchableOpacity style={styles.botao} onPress={this.vai}>
-                <Text style={styles.botaoTexto}>{this.state.botao}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.botao} onPress={this.limpar}>
-                <Text style={styles.botaoTexto}>LIMPAR</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.linha}>
+        </View>
+        <View style={styles.linha}>
+          <Botao cols="3" n="C" />
+          <Botao bg="#fd9526" n="/" />
+        </View>
+        <View style={styles.linha}>
+          <Botao n="7"/>
+          <Botao n="8"/>
+          <Botao n="9"/>
+          <Botao bg="#fd9526" n="*"/>
+        </View>
+        <View style={styles.linha}>
+          <Botao n="4"/>
+          <Botao n="5"/>
+          <Botao n="6"/>
+          <Botao bg="#fd9526" n="-"/>
+        </View>
+        <View style={styles.linha}>
+        <Botao n="1"/>
+          <Botao n="2"/>
+          <Botao n="3"/>
+          <Botao bg="#fd9526" n="+"/>
+        </View>
+        
+        <View style={styles.linha}>
+          <Botao cols="2" n="0"/>
+          <Botao bg="#fd9526" n="."/>
+          <Botao bg="#fd9526" n="="/>
+        </View>
+        
       </View>
     )
   }
@@ -65,38 +84,16 @@ const styles = StyleSheet.create({
   body:{
     paddingTop:20,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c1f30',
   },
-  imagem:{
-    
-  }, 
-  tempoText:{
-    fontSize:80,
-    fontWeight:'bold',
-    color:'#baa07a',
-    //textAlign: 'center',
-    marginTop: -150,
-  },
-  botaoArea:{
+  linha:{
+    flex: 1,
     flexDirection: 'row',
-    height: 40,
-    marginTop: 80
   },
   botao:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor:'#886532',
-    height:40,
-    borderRadius: 5,
-    margin:10
+   
   },
   botaoTexto:{
-    fontSize: 17,
-    color: '#fff',
-    fontWeight: 'bold',
+
   }
   
 })
